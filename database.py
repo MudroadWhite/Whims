4f70960d6199645042439779e1d068da06d725de
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2022-03-04 01:34:50 -0600

    Refactored according to some tutorial

--- database.py
+++ database.py
@@ -2,10 +2,11 @@ import mysql.connector
 from bcrypt import checkpw, hashpw, gensalt
 
 db_username = ""
-db_password = ""
+db_passwd = ""
 db_dbname = ""
 
 # TODO: make a class
+# TODO: (IMPORTANT) mydb.close()
 mydb = mysql.connector.connect(
     host="localhost",
     user=db_username,
@@ -14,6 +15,9 @@ mydb = mysql.connector.connect(
 )
 
 my_cursor = mydb.cursor(buffered=True)
+
+def get_database():
+    return mydb
 
 
 def create_users_table():
@@ -30,9 +34,17 @@ def register_user(name, passwd):
     mydb.commit()
 
 
+def check_user_exists(name):
+    """Check if a user already exists in the database"""
+    query = "SELECT * FROM users WHERE username = '{u}'".format(u=name)
+    my_cursor.execute(query)
+    count = len(my_cursor.fetchall())
+    return True if count >= 0 else False
+
+
 def login(name, passwd):
     """Check if a passwd matches the username in the database"""
-    query = "SELECT * FROM users WHERE username = '{u}' AND passwd = '{p}'".format(u=username, p=passwd)
+    query = "SELECT * FROM users WHERE username = '{u}' AND passwd = '{p}'".format(u=name, p=passwd)
     my_cursor.execute(query)
     count = len(my_cursor.fetchall())
     return True if count >= 0 else False
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 import mysql.connector
 from flask import Flask, render_template, request, flash, session, redirect, url_for
 from flask_session import Session
+from auth import bp
 from bcrypt import hashpw, gensalt, checkpw
 # from database import get_event_name, create_request_row, \
 #     delete_request, create_pledge_row, create_donation_row, expire_request
@@ -55,82 +56,38 @@ def home():
     """显示首页"""
     return render_template("index.html")
 
-@app.route('/showSignIn')
-def show_sign_in():
-    """渲染登录页面"""
-    return render_template("signin.html")
-
-@app.route('/showSignUp')
-def show_sign_up():
-    """渲染注册页面"""
-    return render_template("signup.html")
-
-@app.route('/showSignUp', methods=['POST'])
-def get_sign_up():
-    username = request.form["username"]
-    psw = request.form["psw"]
-    conf_psw = request.form["conf_psw"].encode('utf-8')
-    data = [username, psw, conf_psw]
-
-    if psw == conf_psw:
-        my_cursor = mydb.cursor(buffered=True)
-        my_cursor.execute("SELECT * FROM Users WHERE username = '{}'".format(username))
-        count = len(my_cursor.fetchall())
-        if count == 0:
-            # Account does not exist
-            sql = "INSERT INTO users (username, psw) VALUES (%s, %s)"
-            val = (username, hashpw(psw, gensalt(12)))
-            my_cursor.execute(sql, val)
-            mydb.commit()
-            flash("注册成功")
-            # TODO: 找个个人网页
-            return render_template("index.html")
-
-        else:
-            # Account does exist
-            flash("账号已存在")
-            return render_template("signUp.html")  # , data=data)
-    flash("密码不相同")
-    return render_template("index.html")
-
-@app.route('/showSignIn', methods=['POST'])
-def get_sign_in():
-    """尝试登陆"""
-    pass
-#     # # Get info from form
-#     # email = request.form['email']
-#     # psw = request.form['psw'].encode('utf-8')
-#     # db_name = None
-#     #
-#     # # query database table users for info
-#     # my_cursor = mydb.cursor(buffered=True)
-#     # my_cursor.execute("SELECT * FROM users WHERE email = '{}'".format(email))
-#     # db_result = my_cursor.fetchall()
-#     # for i in db_result:
-#     #     db_pass = i[3].encode('utf-8')
-#     # # check that passwords match
-#     # if db_pass is not None:
-#     #     if checkpw(psw, db_pass):
-#     #         # Passwords match
-#     #         session.permanent = False
-#     #         session['name'] = db_name
-#     #         session['login'] = True
-#     #         return render_template("index.html")
-#     #
-#     # # Passwords don't match or user doesn't exist
-#     # flash("Username or password is incorrect")
-#     return render_template("signIn.html")
-
-# @app.route('/logout')
-# def logout():
-#     """注销账户"""
-#     '''session['name'] = None
-#     session['login'] = False'''
-#     session.clear()
+# @app.route('/showSignUp', methods=['POST'])
+# def get_sign_up():
+#     username = request.form["username"]
+#     psw = request.form["psw"]
+#     conf_psw = request.form["conf_psw"].encode('utf-8')
+#     data = [username, psw, conf_psw]
+#
+#     if psw == conf_psw:
+#         # TODO: switch to database.py functions
+#         my_cursor = mydb.cursor(buffered=True)
+#         my_cursor.execute("SELECT * FROM Users WHERE username = '{}'".format(username))
+#         count = len(my_cursor.fetchall())
+#         if count == 0:
+#             # Account does not exist
+#             sql = "INSERT INTO users (username, psw) VALUES (%s, %s)"
+#             val = (username, hashpw(psw, gensalt(12)))
+#             my_cursor.execute(sql, val)
+#             mydb.commit()
+#             flash("注册成功")
+#             # TODO: 找个个人网页
+#             return render_template("index.html")
+#
+#         else:
+#             # Account does exist
+#             flash("账号已存在")
+#             return render_template("signUp.html")  # , data=data)
+#     flash("密码不相同")
 #     return render_template("index.html")
 
 def main():
     """Defines main function to be used by automated testing"""
+    app.register_blueprint(bp)
     app.run()
     mydb.close()
 
