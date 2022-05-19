edf9f3c98f682340ef7c063131390d8555fb5a02
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2022-05-19 12:11:47 -0500

    Learning Socket Protocol

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
+# For learning purpose
+
 import socket
 
+# TODO: make client also send server messages
+
 def client():
+    # 0. Socket settings
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 1. `CONNECT` to some remote addr and port
     s.connect((socket.gethostname(), 1234))
+    # 2. Wait for server to accept the connection
 
     while True:
         full_msg = ''
         while True:
+            # 3. Make a buffer of 1024. Received messages will be stored in the buffer
+            # TODO: is that a locked operation?
             msg = s.recv(8)
             if len(msg) <= 0:
                 break
--- server.py
+++ server.py
@@ -1,15 +1,30 @@
+# For learning purpose
+
 import socket
 
 # https://pythonprogramming.net/sockets-tutorial-python-3/
+# https://www.ibm.com/docs/en/zos/2.4.0?topic=tcpip-basic-socket-calls
+# https://www.youtube.com/watch?v=XXfdzwEsxFk
+
+# TODO: make client also send server messages
+
 def server():
+    # 0. Set the configuration
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 1. `BIND` to a local port address
+    # TODO: why get host name?
     s.bind((socket.gethostname(), 1234))
+    # 3. Start LISTENing
     s.listen(5)
 
     while True:
-        # now our endpoint knows about the OTHER endpoint.
+        # 4. ACCEPT connection. Accept will wait until a `CONNECT` request has been received
+        # The weird new socket seems to be part of the default specification of the ACCEPT method
+        # On server's side, two sockets has bound to the same port
+        # TODO: how many sockets can be bound to a port at server/client side? Unlimited?
         clientsocket, address = s.accept()
         print(f"Connection from {address} has been established.")
+        # 5. Continuously send messages to client side
         clientsocket.send(bytes("Hey there!!!","utf-8"))
         clientsocket.close()
 
